856b2105329751b547be50c6cd751e5198517e3f
Jose Murillo <jooseemurillo@gmaill.com>
2023-01-02 15:57:10 -0300

    feat: build ui to component header without logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,16 @@
 import * as S from "./styles";
 import { HeaderProps } from "./types";
 
-const Header: React.FC = (props: HeaderProps) => {
-  return <S.ContainerHeader>
-    <h1>Header</h1>
-  </S.ContainerHeader>;
+export default function Header (props: HeaderProps) {
+  return (
+    <S.ContainerHeader>
+      <div className="logo" />
+
+      <S.ProfileHeader>
+         <img src='https://i.ibb.co/fpcxPz4/Janet.png' />
+         <span>User name</span>
+      </S.ProfileHeader>
+    </S.ContainerHeader>
+  );
 }
 
-export default Header;
