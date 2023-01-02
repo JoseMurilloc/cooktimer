da74dbd0c0ace9a363e56b6939031fa58a49f129
Jose Murillo <jooseemurillo@gmail.com>
2023-01-02 18:34:41 -0300

    doc: add colors the application

---
+++ src/stories/docs/components/ColorsGrid.tsx
@@ -0,0 +1,21 @@
+import { colors } from '../../designSystems/colors'
+
+export function ColorsGrid() {
+  return Object.entries(colors).map(([key, color]) => {
+    return (
+      <div key={key} style={{ backgroundColor: color, padding: '2rem' }}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            fontFamily: 'monospace',
+            color: '#000'
+          }}
+        >
+          <strong>${key}</strong>
+          <span>{color}</span>
+        </div>
+      </div>
+    )
+  })
+}
