41e98443db6f4c8cbb21c3fb549695ada27fa30d
Jose Murillo <jooseemurillo@gmaill.com>
2023-01-02 15:41:02 -0300

    feat: start build interface of home page

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -1,6 +1,29 @@
 import { styled } from "..";
 
 export const Container = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  width: '100vw',
   backgroundColor: '#fff',
-  height: '100vh',
+  paddingTop: '$10',
+  gap: '$20',
+})
+
+export const Main = styled('main', {
+  width: '70rem',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '$10',
+
+  'section h1': {
+    fontWeight: '$bold',
+    fontSize: '$5xl',
+    color: '$text',
+  },
+
+  'section p': {
+    color: '$text_400',
+    fontSize: '$md',
+  }
 })
