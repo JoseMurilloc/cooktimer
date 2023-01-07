e1191e7f5917713396dd6fab0086c04010b75d4a
Jose Murillo <jooseemurillo@gmail.com>
2023-01-07 14:51:34 -0300

    feat: create select emoji component

---
+++ src/components/SelectedFoodEmoji/index.tsx
@@ -0,0 +1,64 @@
+import { CaretDown } from 'phosphor-react'
+import { colors } from 'stories/designSystems/colors'
+
+import * as SelectPrimitive from '@radix-ui/react-select'
+import * as S from './styles'
+import Image from 'next/image'
+
+const OPTIONS = [
+  {
+    contentURL: '/pudim.svg',
+    name: 'Bolo',
+    value: 'cake'
+  },
+  {
+    contentURL: '/frigideira.svg',
+    name: 'Frigideira',
+    value: 'skillet'
+  },
+  {
+    contentURL: '/panelaPressao.svg',
+    name: 'Panela pressão',
+    value: 'pressureCooker'
+  },
+  {
+    contentURL: '/panela.svg',
+    name: 'Panela',
+    value: 'pan'
+  },
+]
+
+export function SelectedFoodEmoji ()  {
+  return (
+    <SelectPrimitive.Root>
+      <S.Trigger defaultValue="cake" >
+        <SelectPrimitive.Value placeholder="Selecione 😋" />
+        <SelectPrimitive.Icon>
+          <CaretDown
+            color={colors.text_600}
+            size={15} 
+            weight="fill" 
+          />
+        </SelectPrimitive.Icon>
+      </S.Trigger>
+      <SelectPrimitive.Portal>
+        <S.Content>
+          <SelectPrimitive.Viewport>
+            {OPTIONS.map(({value, contentURL, name}) => (
+              <SelectPrimitive.Item key={value} value={value}>
+                <SelectPrimitive.SelectItemText>
+                  <Image 
+                    src={contentURL} 
+                    width={25} 
+                    height={25} 
+                    alt="image" 
+                  />
+                </SelectPrimitive.SelectItemText>
+              </SelectPrimitive.Item>
+            ))}
+          </SelectPrimitive.Viewport>
+        </S.Content>
+      </SelectPrimitive.Portal>
+    </SelectPrimitive.Root>
+  );
+}
---
+++ src/components/SelectedFoodEmoji/styles.ts
@@ -0,0 +1,29 @@
+import * as Select from '@radix-ui/react-select';
+import { styled } from "../../styles";
+
+export const Trigger = styled(Select.Trigger, {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  
+  maxHeight: '2.7rem',
+  minWidth: 50,
+  padding: '$3 $2',
+    
+  backgroundColor: '$text_000',
+  border: '1px solid $text_400',
+  borderRadius: 10,
+  cursor: 'pointer',
+})
+
+export const Content = styled(Select.Content, {
+  display: 'flex',
+  alignItems: 'center',
+
+  padding: '$3 $2',
+  backgroundColor: '$text_000',
+  border: '1px solid $text_400',
+  borderRadius: 10,
+  cursor: 'pointer',
+  boxShadow: '0px 10px 38px -10px rgba(22, 23, 24, 0.35), 0px 10px 20px -15px rgba(22, 23, 24, 0.2)',
+})
