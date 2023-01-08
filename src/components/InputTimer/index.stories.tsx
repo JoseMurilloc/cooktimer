66a41fc0b715d72d0349f7e314e25b556c51b831
Jose Murillo <jooseemurillo@gmail.com>
2023-01-07 22:02:36 -0300

    doc: create component input timer

---
+++ src/components/InputTimer/index.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { ComponentStory, Meta } from '@storybook/react'
+import { InputTimer } from '.'
+
+export default {
+  title: 'Components/InputTimer',
+  component: InputTimer,
+  decorators: [
+    (Story) => (
+      <div style={{
+        backgroundColor: 'white',
+        borderRadius: 10,
+        height: 300,
+        width: 250,
+        padding: 20,
+      }}>
+        {Story()}
+      </div>
+    ),
+  ],
+} as Meta<typeof InputTimer>
+
+const Template: ComponentStory<typeof InputTimer> = (args) => (
+  <InputTimer {...args} />
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  label: 'Label',
+}
