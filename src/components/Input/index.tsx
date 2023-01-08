cbcf2901c3be434ddf4cb36b86f988c241650c3f
Jose Murillo <jooseemurillo@gmail.com>
2023-01-08 13:10:37 -0300

    feat: add register name to input to integrate with hook form

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,13 +2,17 @@ import * as S from './styles'
 import { TextFieldProps } from "./types";
 import { WarningCircle } from "phosphor-react";
 import { colors } from "../../stories/designSystems/colors";
+import { useFormContext } from 'react-hook-form';
 
-export function Input ({label, hasError, ...props}: TextFieldProps) {
+export function Input ({registerName, label, hasError, ...props}: TextFieldProps) {
+  const { register } = useFormContext()
+
   return (
     <S.ContainerInput>
       <S.Input 
         hasError={hasError} 
         {...props} 
+        {...register(registerName)}
       />
       <label>{label}</label>
       {hasError && <WarningCircle size={20} weight="fill" color={colors.error} />}
--- src/components/Input/types.ts
+++ src/components/Input/types.ts
@@ -2,6 +2,7 @@ import { InputHTMLAttributes } from "react";
 
 export type TextFieldProps = {
   label: string,
+  registerName: string, 
   hasError?: boolean,
 } & InputHTMLAttributes<HTMLInputElement>;
 
