e46d8bbe199b2d5f3a5bc91e2d3277fa204c1fdc
Jose Murillo <jooseemurillo@gmail.com>
2023-01-08 13:10:51 -0300

    feat: add register name to input timer to integrate with hook form

--- src/components/InputTimer/index.tsx
+++ src/components/InputTimer/index.tsx
@@ -1,17 +1,23 @@
 import * as S from './styles'
 import { InputTimerProps } from './types'
+import { useFormContext } from 'react-hook-form'
+import { NewTimerFormData } from 'components/AddPlayerPopover/types'
 
 export function InputTimer ({label}: InputTimerProps) {
+  const { register, formState: { errors } } = useFormContext<NewTimerFormData>()
+  
   return (
     <S.ContainerInputTimer>
       <label>{label}</label>
       <S.ContainerInputs>
         <section className="input">
           <S.Input
+            hasError={!!errors.timerHour}
             placeholder='00' 
             type="number" 
             min={0}
             max={24}
+            {...register('timerHour', {valueAsNumber: true})}
           />
           <span>Horas</span>
         </section>
@@ -21,11 +27,12 @@ export function InputTimer ({label}: InputTimerProps) {
         </S.SeparatorDot>
         <section className="input">
           <S.Input
+            hasError={!!errors.timerMinutes}
             placeholder='20' 
             type="number" 
-            step={5}
             min={0}
             max={59}
+            {...register('timerMinutes', {valueAsNumber: true})}
           />
           <span>Minutos</span>
         </section>
