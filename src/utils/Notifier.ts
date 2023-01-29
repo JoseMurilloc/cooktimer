62b1c301ed5e9b98449115e05e33b3d9cba6d5f1
Jose Murillo <jooseemurillo@gmail.com>
2023-01-29 14:26:41 -0300

    feat: create Util to handle with notifications

---
+++ src/utils/Notifier.ts
@@ -0,0 +1,28 @@
+type NotifyParams = {
+  title: string
+  body: string
+  icon?: string
+}
+
+const Notifier = {
+  async init () {
+    const permission = await Notification.requestPermission()
+    if (permission !== 'granted') {
+      throw new Error('Permissão negada')
+    }
+  },
+  async playSound (url: string) {
+    const audio = new Audio(url)
+    await audio.play()
+  },
+  async notify ({ title, body, icon }: NotifyParams) {
+    const notification = new Notification(title, {
+      body,
+      icon
+    })
+
+    console.log(notification)
+  }
+}
+
+export { Notifier }
