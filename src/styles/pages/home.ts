1b904fe636a6b45379f527c07eee3063c4acf882
Jose Murillo <jooseemurillo@gmail.com>
2023-05-19 19:15:32 -0300

    refactor: clean home screen

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -1,32 +1,8 @@
 import { styled } from '..'
 
-export const Container = styled('div', {
+export const ContainerHome = styled('div', {
   display: 'block',
-  width: '100vw'
-})
-
-export const Main = styled('main', {
-  maxWidth: '70rem',
-  margin: 'auto',
-  display: 'flex',
-  flexDirection: 'column',
-  gap: '$10',
-
-  'section p': {
-    color: '$text_600',
-    fontSize: '$md'
-  },
-
-  'section > h1': {
-    fontSize: '$6xl',
-    fontWeight: '$bold',
-    width: 500,
-    color: '$text_600'
-  },
-
-  '.cardsGrid': {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(4, 1fr)',
-    gap: '$5'
-  }
+  width: '100vw',
+  height: '100vh',
+  backgroundColor: '$text_000'
 })
