65858c351298bf04c239d53705c19f1bebac0539
Jose Murillo <jooseemurillo@gmail.com>
2023-05-19 18:36:56 -0300

    feature: create timer page

---
+++ src/pages/timer/styles.ts
@@ -0,0 +1,32 @@
+import { styled } from '../../styles'
+
+export const Container = styled('div', {
+  display: 'block',
+  width: '100vw'
+})
+
+export const Main = styled('main', {
+  maxWidth: '70rem',
+  margin: 'auto',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '$10',
+
+  'section p': {
+    color: '$text_600',
+    fontSize: '$md'
+  },
+
+  'section > h1': {
+    fontSize: '$6xl',
+    fontWeight: '$bold',
+    width: 500,
+    color: '$text_600'
+  },
+
+  '.cardsGrid': {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(4, 1fr)',
+    gap: '$5'
+  }
+})
